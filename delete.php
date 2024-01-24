ba435b62217eac12655fcb8ec5831a08a9c040ba
Sirikorn khamthai <6431503106@lamduan.mfu.ac.th>
2024-01-25 00:20:10 +0700

    delete

--- delete.php
+++ delete.php
@@ -1,21 +1,10 @@
 <?php
-include "config.php";
+include("config.php");
+include("firebaseRDB.php");
 
-if (isset($_GET['id']) && is_numeric($_GET['id'])) {
-    $product_id = $_GET['id'];
-
-    $delete_query = "DELETE FROM product1 WHERE id = $product_id";
-    $result = mysqli_query($conn, $delete_query);
-
-    if ($result) {
-        header("Location: index.php?msg=Product deleted successfully");
-        exit();
-    } else {
-        echo "Error deleting product: " . mysqli_error($conn);
-        exit();
-    }
-} else {
-    echo "Invalid product ID.";
-    exit();
+$db = new firebaseRDB($databaseURL);
+$id = $_GET['id'];
+if($id != ""){
+   $delete = $db->delete("product", $id);
+   echo "data deleted";
 }
-?>
